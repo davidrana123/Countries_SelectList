fcbee72d132c4cb7b32c0b260500d2167e2eb6b7
David <david123rana@gmail.com>
2021-10-01 06:06:41 -0600

    display data form select

--- src/App.js
+++ src/App.js
@@ -5,44 +5,102 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "coconut",
-      lists: ["india", "cnada", "usa"],
+      courses: [],
+      course: "",
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmitCourse = this.handleSubmitCourse.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    alert("Your favorite flavor is: " + this.state.value);
+  handleSubmitCourse(event) {
+    alert("Your selected value is: " + this.state.course);
     event.preventDefault();
   }
 
+  handleChangeCourse = (event) => {
+    this.setState({ course: event.target.value });
+  };
+
+  getUnique(arr, comp) {
+    const unique = arr
+      //store the comparison values in array
+      .map((e) => e[comp])
+
+      // store the keys of the unique objects
+      .map((e, i, final) => final.indexOf(e) === i && i)
+
+      // eliminate the dead keys & store unique objects
+      .filter((e) => arr[e])
+
+      .map((e) => arr[e]);
+
+    return unique;
+  }
+
+  // componentDidMount() {
+  //   const courses = require("./courses.json");
+  //   this.setState({ courses: courses });
+  // }
+
+  componentDidMount() {
+    fetch("https://restcountries.com/v2/all").then((resp) => {
+      resp.json().then((courses) => {
+        // console.warn (result)
+        this.setState({ courses: courses });
+      });
+    });
+  }
+
   render() {
+    const uniqueCouse = this.getUnique(this.state.courses, "tag");
+
+    const courses = this.state.courses;
+    const course = this.state.course;
+
+    const filterDropdown = courses.filter(function (result) {
+      return result.name === course;
+    });
+
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Pick your favorite flavor:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">Grapefruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-            <option value="mango">Mango</option>
-          </select>
-        </label>
-        <select>
-          {this.state.lists.map((list) => (
-            <option key={list} value={list}>
-              {list}
-            </option>
-          ))}
-        </select>
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmitCourse}>
+          <br />
+          <br />
+          <label>
+            Looping through Courses tag from Json File
+            <select
+              value={this.state.course}
+              onChange={this.handleChangeCourse}
+            >
+              {this.state.courses.map((course) => (
+                <option key={course.name} value={course.name}>
+                  {course.name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <input type="submit" value="Submit" />
+          <div>
+            {filterDropdown.map((course) => (
+              <div key={course.name} style={{ margin: "10px" }}>
+                <div id="flag-container">
+                  <img src={course.flags} alt="" />
+                </div>
+                <label>Name: {course.name}</label>
+                <br />
+                <label>region: {course.region}</label>
+                <br />
+                <label>population: {course.population}</label>
+                <br />
+                <label>capital: {course.capital}</label>
+                <br />
+                <label>currencies: {course.currencies[1]}</label>
+                <br />
+              </div>
+            ))}
+          </div>
+        </form>
+      </div>
     );
   }
 }
